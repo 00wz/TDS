43d7fda79920a9c9c4428b39a2ae47d46789881e
Evgen <jetset.3566@gmail.com>
2020-05-07 03:59:52 +0600

    Anim + Enviro Level 1

--- Source/TPS/Game/TPSGameMode.cpp
+++ Source/TPS/Game/TPSGameMode.cpp
@@ -11,7 +11,7 @@ ATPSGameMode::ATPSGameMode()
 	PlayerControllerClass = ATPSPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprint/Character/TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprint/Character/BP_Character"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
