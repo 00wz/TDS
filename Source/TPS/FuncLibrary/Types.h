4a332339fd4d36b20841f526bd41785f7e6b3324
Evgen_NoteBook <jetset.3566@gmail.com>
2020-06-11 22:48:38 +0600

    add table weapon

--- Source/TPS/FuncLibrary/Types.h
+++ Source/TPS/FuncLibrary/Types.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "Engine/DataTable.h"
+
 #include "Types.generated.h"
 
 UENUM(BlueprintType)
@@ -41,21 +43,84 @@ struct FProjectileInfo
 	TSubclassOf<class AProjectileDefault> Projectile = nullptr;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ProjectileSetting")
 	float ProjectileDamage = 20.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ProjectileSetting")
+	float ProjectileLifeTime = 20.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ProjectileSetting")
+	float ProjectileInitSpeed = 2000.0f;
+
+	//Hit fx Actor?
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ProjectileSetting")
+		bool bIsLikeBomp = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ProjectileSetting")
+	float ProjectileMaxRadiusDamage = 200.0f;
 };
 
 USTRUCT(BlueprintType)
-struct FWeaponInfo
+struct FWeaponDispersion
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting")
-	float WeaponDamage = 20.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		float DispersionAimStart = 0.5f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		float DispersionAimMax = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		float DispersionAimMin = 0.1f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		float DispersionAimShootCoef = 1.0f;
+};
+
+USTRUCT(BlueprintType)
+struct FWeaponInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Class")
+	TSubclassOf<class AWeaponDefault> WeaponClass = nullptr;
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting")
 	float RateOfFire = 0.5f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting")
+	float ReloadTime = 2.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting")
+	int32 MaxRound = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		FWeaponDispersion DispersionWeapon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		USoundBase* SoundFireWeapon = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		USoundBase* SoundReloadWeapon = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		UParticleSystem* EffectFireWeapon = nullptr;
+	////if null use trace logic (TSubclassOf<class AProjectileDefault> Projectile = nullptr)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
 	FProjectileInfo ProjectileSetting;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		float WeaponDamage = 20.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		float DistacneTrace = 2000.0f;
+	//one decal on all?
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		UDecalComponent* DecalOnHit = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		UAnimMontage* AnimCharFire = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting ")
+		UAnimMontage* AnimCharReload = nullptr;
 };
 
+USTRUCT(BlueprintType)
+struct FAddicionalWeaponInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	int32 Round = 10;
+};
 
 UCLASS()
 class TPS_API UTypes : public UBlueprintFunctionLibrary
---
+++ Source/TPS/Game/TPSGameInstance.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSGameInstance.h"
+
+FWeaponInfo UTPSGameInstance::GetWeaponInfoByName(FName NameWeapon)
+{
+	FWeaponInfo WeaponInfo;
+	return WeaponInfo;
+}
