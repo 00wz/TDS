cd58f872cde52ee00b6444b12c135acfa523cc38
00wz <130168402+00wz@users.noreply.github.com>
2024-01-27 09:08:24 +0300

    homework7.10

--- Source/TPS/TPS.Build.cs
+++ Source/TPS/TPS.Build.cs
@@ -8,6 +8,7 @@ public class TPS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "PhysicsCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
+	        "PhysicsCore", "Slate", "HeadMountedDisplay", "NavigationSystem", "AIModule" });
     }
 }
